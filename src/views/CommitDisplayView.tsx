c109e3be899855e48832d9d00c1b4b921720cdc7
Eric Myllyoja <ericmyllyoja@gmail.com>
2022-01-24 21:13:01 -0500

    Style cleanup, switch to FlatList for optimized list display.

--- src/views/CommitDisplayView.tsx
+++ src/views/CommitDisplayView.tsx
@@ -1,71 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  SectionList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { TextInput, Button } from "react-native";
-import { COMMIT_COUNT, DEFAULT_REPO, DEFAULT_USER, getRepoCommits, getRepoData, HASH_LENGTH } from "../api/GithubAPI";
-import { GithubRepoData, GithubRepoCommitData, GithubRepoCommit } from "../api/Types";
+import {
+  COMMIT_COUNT,
+  DEFAULT_REPO,
+  DEFAULT_USER,
+  getRepoCommits,
+  getRepoData,
+  HASH_LENGTH,
+} from "../api/GithubAPI";
+import {
+  GithubRepoData,
+  GithubRepoCommitData,
+  GithubRepoCommit,
+} from "../api/Types";
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    backgroundColor: "#fff",
-    // Center items.
-    alignItems: "center",
-    justifyContent: "flex-start",
-    // Padding on sides.
-    width: '60%',
-    height: '100%',
-    margin: 'auto',
-  },
-  rowFull: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-
+  padBottom: {
     marginBottom: 10,
-    flexGrow: 0,
-    minHeight: 24,
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
   columnLeft: {
     justifyContent: "flex-start",
-
-    marginBottom: 10,
-    minHeight: 24,
   },
   rowCenter: {
-    flex: 1,
-    flexDirection: "row",
     justifyContent: "center",
-
-    marginBottom: 10,
-    flexGrow: 0,
-    minHeight: 32,
-  },
-
-  rowCommit: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    textAlign: "center",
     alignItems: "center",
-    marginTop: 10,
-    marginBottom: 10,
-    flexShrink: 0,
-    flexGrow: 1,
   },
-  containerCommit: {
-    display: 'flex',
-    flexGrow: 1,
-    flexShrink: 0,
-  },
+
   textField: {
-    border: "1px solid black",
+    borderWidth: 1,
+    borderColor: "black",
     borderRadius: 5,
+    width: 240,
     marginLeft: 10,
     backgroundColor: "#999",
-  },
-  rowEnd: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
   },
 });
 
@@ -73,7 +59,7 @@ const CommitDisplayView = function () {
   const [inputUser, onChangeInputUser] = React.useState(DEFAULT_USER);
   const [inputRepo, onChangeInputRepo] = React.useState(DEFAULT_REPO);
 
-  const [repoDesc, setRepoDesc] = React.useState('No repo selected');
+  const [repoDesc, setRepoDesc] = React.useState("No repo selected");
 
   const [commits, setCommits] = React.useState<Array<GithubRepoCommit>>([]);
 
@@ -94,11 +80,12 @@ const CommitDisplayView = function () {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.rowFull}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar animated={true} backgroundColor="#333" hidden={false} />
+      <View style={[styles.padBottom]}>
         <Text>Github Commits</Text>
       </View>
-      <View style={styles.rowFull}>
+      <View style={[styles.padBottom, styles.flexRow]}>
         <Text>Username</Text>
         <TextInput
           style={styles.textField}
@@ -106,7 +93,7 @@ const CommitDisplayView = function () {
           value={inputUser}
         />
       </View>
-      <View style={styles.rowFull}>
+      <View style={[styles.padBottom, styles.flexRow]}>
         <Text>Repository</Text>
         <TextInput
           style={styles.textField}
@@ -114,28 +101,38 @@ const CommitDisplayView = function () {
           value={inputRepo}
         />
       </View>
-      <View style={styles.rowCenter}>
+      <View style={[styles.padBottom, styles.rowCenter]}>
         <Button title="Fetch" onPress={onClickFetch} />
       </View>
-      <View style={styles.containerCommit}>
-          <View style={styles.rowCenter}>
-            <Text>{repoDesc}</Text>
-          </View>
-          <View style={styles.rowCenter}>
-            <Text>Commit Messages</Text>
-          </View>
-          {commits.map(({sha, commit: {author: {date}, message}}) => (
-            <View style={styles.columnLeft}>
-              <Text>{sha.substring(0, HASH_LENGTH)} : {date}</Text>
-              {
-                // Correctly display commit messages with multiple lines.
-                message.split("\n").filter(line => line.length > 0)
-                  .map(line => <Text>{line}</Text>)
-              }
-            </View>
-          ))}
+      <Text style={[styles.padBottom, styles.rowCenter]}>{repoDesc}</Text>
+      <Text style={[styles.padBottom, styles.rowCenter]}>Commit Messages</Text>
+      <FlatList
+        data={commits}
+        keyExtractor={(item) => item.sha.substring(0, HASH_LENGTH)}
+        renderItem={({ item: {
+          sha,
+          commit: {
+            author: { date },
+            message,
+          },
+        }}) => (
+          <View key={sha} style={[styles.padBottom, styles.columnLeft]}>
+          <Text>
+            {sha.substring(0, HASH_LENGTH)} : {date}
+          </Text>
+          {
+            // Correctly display commit messages with multiple lines.
+            message
+              .split("\n")
+              .filter((line) => line.length > 0)
+              .map((line) => (
+                <Text key={line}>{line}</Text>
+              ))
+          }
         </View>
-    </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
